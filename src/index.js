eb03b633a09948a002a56f6359898584ad8b4059
yamma10 <122512165+yamma10@users.noreply.github.com>
2023-02-02 14:45:12 +0900

    Update index.js
    corsの設定

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './state/AuthContext';
 
+import axios from 'axios';
+
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
