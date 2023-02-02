360ae4c5b5b943f4f2ae64d33495929cce328926
yamma10 <yamtt1326@gmail.com>
2023-02-02 19:18:41 +0900

    proxy設定

--- src/actionCalls.js
+++ src/actionCalls.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const studentLoginCall = async (user, dispatch) => {
   dispatch({ type: "LOGIN_START" });
   try {
-    const response = await axios.post("https://matchingapi.onrender.com/api/auth/student/login",user);
+    const response = await axios.post("auth/student/login",user);
     dispatch({ type: "LOGIN_SUCCESS", payload: response.data });
   } catch (err) {
     dispatch({type: "LOGIN_ERROR", payload: err })
